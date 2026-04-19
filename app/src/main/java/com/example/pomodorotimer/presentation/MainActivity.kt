6a131c3d4cdfe8c3e6ae8b61783d5357bf2e8ac9
Matt Smith <matt_smith764@live.com>
2026-04-19 10:53:03 +0100

    Fix CI

--- app/src/main/java/com/example/pomodorotimer/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/presentation/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
